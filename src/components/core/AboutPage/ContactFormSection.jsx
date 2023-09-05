ad17d9521fabf1d657d2624d945809e24fd1d8e8
Atulsrj <atulsinghsrj@gmail.com>
2023-09-05 14:59:20 +0530

    modified ContactFormSection code

--- src/components/core/AboutPage/ContactFormSection.jsx
+++ src/components/core/AboutPage/ContactFormSection.jsx
@@ -3,11 +3,9 @@ import ContactUsForm from "../../ContactPage/ContactUsForm";
 
 const ContactFormSection = () => {
   return (
-<<<<<<< HEAD
+
     <div className="border border-richblack-600 text-richblack-300 rounded-xl p-12 lg:p-14 flex gap-3 flex-col mx-auto w-full">
-=======
-    <div className="mx-auto">
->>>>>>> origin/main
+
       <h1 className="text-center text-4xl font-semibold">Get in Touch</h1>
       <p className="text-center text-richblack-300 mt-3">
         We&apos;d love to here for you, Please fill out this form.
